ecf2dccfed0ff5422f832e9aefc4c291e43953fd
Abdelrahman Hamdy <138339608+AbdelrahmanHamdyDev@users.noreply.github.com>
2025-01-24 03:25:07 +0200

    First Missing

---
+++ android/app/src/main/java/com/example/randomfact/RandomFact.kt
@@ -0,0 +1,50 @@
+package com.example.randomfact
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.widget.RemoteViews
+import es.antonborri.home_widget.HomeWidgetPlugin
+
+/**
+ * Implementation of App Widget functionality.
+ */
+class RandomFact : AppWidgetProvider() {
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        // There may be multiple widgets active, so update all of them
+        for (appWidgetId in appWidgetIds) {
+            val widgetData = HomeWidgetPlugin.getData(context)
+            val views = RemoteViews(context.packageName,R.layout.random_fact).apply{
+                val textFromFlutterApp = widgetData.getString("randomFact_Widget",null)
+                setTextViewText(R.id.text_id,textFromFlutterApp?:"No text...")
+            }
+            appWidgetManager.updateAppWidget(appWidgetId,views)
+        }
+    }
+
+    override fun onEnabled(context: Context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    override fun onDisabled(context: Context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
+internal fun updateAppWidget(
+    context: Context,
+    appWidgetManager: AppWidgetManager,
+    appWidgetId: Int
+) {
+    val widgetText = context.getString(R.string.appwidget_text)
+    // Construct the RemoteViews object
+    val views = RemoteViews(context.packageName, R.layout.random_fact)
+    views.setTextViewText(R.id.text_id, widgetText)
+
+    // Instruct the widget manager to update the widget
+    appWidgetManager.updateAppWidget(appWidgetId, views)
+}
